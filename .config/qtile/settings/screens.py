bf1bc130dff3a46ae2a634b52594ce822c25bc84
sergiorg98 <sergioramiro98@gmail.com>
2021-10-30 19:11:25 +0200

    multiple monitor support

--- .config/qtile/settings/screens.py
+++ .config/qtile/settings/screens.py
@@ -1,35 +1,32 @@
 from libqtile.config import Screen
 from libqtile import bar
-from libqtile import widget
+from libqtile.log_utils import logger
+from .widgets import primary_widgets, secondary_widgets
+import subprocess
 
-screens = [
-    Screen(
-        top=bar.Bar(
-            [
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.CurrentLayout(),
-                widget.Systray(),
-                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
-            ],
-            24,
-            opacity=0.90,
-        ),
-    ),
 
-    Screen(
-        bottom=bar.Bar(
-            [
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.CurrentLayout(),
-                widget.Systray(),
-                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
-            ],
-            24,
-            opacity=0.90,
-        ),
-    ),
-]
+def status_bar(widgets):
+    return bar.Bar(widgets, 24, opacity=0.92)
+
+
+screens = [Screen(top=status_bar(primary_widgets))]
+
+xrandr = "xrandr | grep -w 'connected' | cut -d ' ' -f 2 | wc -l"
+
+command = subprocess.run(
+    xrandr,
+    shell=True,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+)
+
+if command.returncode != 0:
+    error = command.stderr.decode("UTF-8")
+    logger.error(f"Failed counting monitors using {xrandr}:\n{error}")
+    connected_monitors = 1
+else:
+    connected_monitors = int(command.stdout.decode("UTF-8"))
+
+if connected_monitors > 1:
+    for _ in range(1, connected_monitors):
+        screens.append(Screen(top=status_bar(secondary_widgets)))
--- .config/qtile/settings/widgets.py
+++ .config/qtile/settings/widgets.py
@@ -1,3 +1,27 @@
+from libqtile import widget
+
+
+primary_widgets = [
+    widget.GroupBox(),
+    widget.Prompt(),
+    widget.WindowName(),
+    widget.CurrentLayout(),
+    widget.Systray(),
+    widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+
+]
+
+secondary_widgets = [
+    widget.GroupBox(),
+    widget.Prompt(),
+    widget.WindowName(),
+    widget.CurrentLayout(),
+    widget.Systray(),
+    #widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+]
+
+
+
 
 widget_defaults = dict(
     font='Hack Nerd Font"',
